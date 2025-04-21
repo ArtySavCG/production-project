b00be7173d6e5fad37cda23da7b761d695abe902
ArtySavCG <artysavelyev@gmail.com>
2025-04-21 18:54:50 +0300

    add config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+import { BuildOptions } from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+	return [
+		new HtmlWebPackPlugin({
+			template: paths.html,
+		}),
+		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash:8].css',
+			chunkFilename: 'css/[name].[contenthash:8].css',
+		})
+	]
+}
